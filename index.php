b5801215554b0d4002aea2d91ba568b0a35d415b
andriawan <alhaqandriawan@gmail.com>
2020-11-13 08:21:18 +0700

    add icon
    menambahkan gambar icon di index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="andriawan">
+    <meta name="keywords" content="web, hitung">
+    <meta name="description" content="web hitung minimalis">
+    <title>WEB HITUNG | SEDERHANA</title>
+    <link rel="icon" href="">
+  </head>
+  <body>
+
+  </body>
+</html>
